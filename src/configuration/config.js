d434377390b05e215e1c2a4f25b04e6dcb27191d
3945014 <3945014@gmail.com>
2026-06-22 20:15:48 +0300

    Validation

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
     port: process.env.PORT || 3000,
     mongodb: {
-        uri: process.env.MONGO_URI || 'mongodb+srv://3945014_db_user:ccgj5gGwyAqFAn4I@cluster0.s1ysafr.mongodb.net/?appName=Cluster0',
+        uri: process.env.MONGO_URI || 'mongodb+srv://3945014_db_user:@cluster0.s1ysafr.mongodb.net/?appName=Cluster0',
         db: {
             dbname: process.env.DB_NAME || 'cohort82'
         }
